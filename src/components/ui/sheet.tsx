55ffb7e96194edd3873004ce0f13de214b7b4534
Jeric Patiag <jericpatiag16@gmail.com>
2025-09-11 10:37:31 +0000

    now it's correct width but it's still in the left, can you keep it under

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -39,7 +39,7 @@ const sheetVariants = cva(
         container: "absolute"
       },
       side: {
-        top: "top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top w-full max-w-sm mx-auto",
+        top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top max-w-sm mx-auto",
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
