3eb0f0526d4822a0bb3639c11d54336c520f20f1
Jeric Patiag <jericpatiag16@gmail.com>
2025-09-12 07:26:21 +0000

    still cant click it

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -13,7 +13,13 @@ const SheetTrigger = SheetPrimitive.Trigger
 
 const SheetClose = SheetPrimitive.Close
 
-const SheetPortal = SheetPrimitive.Portal
+const SheetPortal = ({
+  className,
+  ...props
+}: SheetPrimitive.DialogPortalProps) => (
+  <SheetPrimitive.Portal className={cn(className)} {...props} />
+)
+SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
@@ -39,7 +45,7 @@ const sheetVariants = cva(
         container: ""
       },
       side: {
-        top: "border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
+        top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
@@ -47,13 +53,6 @@ const sheetVariants = cva(
           "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
       },
     },
-    compoundVariants: [
-      {
-        variant: "container",
-        side: "top",
-        className: "top-0 left-0 right-0 max-w-sm mx-auto",
-      },
-    ],
     defaultVariants: {
       side: "right",
       variant: "default"
@@ -70,15 +69,14 @@ const SheetContent = React.forwardRef<
   SheetContentProps
 >(({ side = "right", variant, className, children, ...props }, ref) => (
   <SheetPortal>
+    {variant !== 'container' && <SheetOverlay />}
     <SheetPrimitive.Content
       ref={ref}
-      className={cn(sheetVariants({ side, variant }), "pointer-events-none", className)}
+      className={cn(sheetVariants({ side, variant }), className)}
       {...props}
     >
-      <div className="pointer-events-auto w-full">
-        {children}
-      </div>
-      <SheetPrimitive.Close className="pointer-events-auto absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      {children}
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
