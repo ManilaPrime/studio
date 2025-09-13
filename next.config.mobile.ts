ecb07de45a8bc06219c0afb45f1f13859639f935
Jeric Patiag <jericpatiag16@gmail.com>
2025-09-13 05:05:35 +0000

    you know what, im gonna delete it so you have nothing to edit on cause y

---
+++ next.config.mobile.ts
@@ -0,0 +1,42 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Exclude the api directory from the build for static export
+  webpack: (config, { isServer }) => {
+    // This is the correct way to ignore a directory during the build.
+    config.module.rules.push({
+        test: /\/app\/api\//,
+        loader: 'ignore-loader',
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
